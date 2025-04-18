eb9a861cb0f123642e5da64fccb93cecd720d894
KenediOne <71275847+KenediOne@users.noreply.github.com>
2025-04-18 14:51:19 +0300

    Revert "Revert "Create""
    This reverts commit 0c12d06836f7b72cc48db981d9aacce1373ffa76.

---
+++ app/src/test/java/com/example/devopsdemo/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsdemo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
